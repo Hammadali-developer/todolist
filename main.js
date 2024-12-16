1e2294d56b36fecba7bbf4f8447a4e14a3cf7c9f
Hammad Ali <developermrhammad@gmail.com>
2024-12-17 03:37:28 +0500

    Todo List Updated.

---
+++ main.js
@@ -0,0 +1,10 @@
+let todolistArry = [];
+let todoList = prompt("Enter Your Task");
+todolistArry.push(todoList);
+console.log(todolistArry);
+
+
+// let data = []; 
+// let userInput = prompt('Enter some data'); 
+// data.push(userInput); 
+// console.log(data);
