be5b3dc11daad046406173383c9ce71723c88e22
Hammad Ali <developermrhammad@gmail.com>
2024-12-17 12:54:12 +0500

    Update main.js
    Comint

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ let todoList2 = prompt("Enter Your Task");
 let todoList3 = prompt("Enter Your Task");
 todolistArry.push(todoList, todoList1, todoList2, todoList3);
 console.log(todolistArry);
+
+
+// if(age >= 18 && country==='pakistan')
+// {
+//  const carName = prompt("Which car you want to drive?")
+//  alert(`you can drive ${carName}`);
+// }
+// else if(age >= 20 && country==='india'){
+//     const carName = prompt("Which car you want to drive?")
+//  alert(`you can drive ${carName}`);
+// } 
+// else{
+//  alert('You are not eligible')   
+// }
