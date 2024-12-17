1000665bc4a0b1d49f23afb7f94051156d8759d9
Hammad Ali <developermrhammad@gmail.com>
2024-12-17 10:30:09 +0500

    Update main.js
    updated

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let todolistArry = [];
 let todoList = prompt("Enter Your Task");
-todolistArry.push(todoList);
+let todoList1 = prompt("Enter Your Task");
+let todoList2 = prompt("Enter Your Task");
+let todoList3 = prompt("Enter Your Task");
+todolistArry.push(todoList, todoList1, todoList2, todoList3);
 console.log(todolistArry);
